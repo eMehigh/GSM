270a0e40ef9761828a65110d8f9d22cff24eccf0
Paraschiva Mihai <mihaieugen233@yahoo.com>
2021-03-03 20:57:24 +0200

    ready for heroku

--- venv/gsm/gsm/settings.py
+++ venv/gsm/gsm/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-
+import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -118,6 +118,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 
@@ -128,3 +129,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'paraschiva.mihai29@gmail.com'
 EMAIL_HOST_PASSWORD = 'SupBruh6'
+
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
